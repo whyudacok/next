fd94014dc58c6529c0739c82e96fae0b3173fc79
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-07-07 04:04:29 +0700

    Update HomePage.tsx

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,21 +1,21 @@
-export interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+export interface Article {
+  link: string;
+  typez: string;
+  title: string;
+  img: string;
 }
 
-export default function HomePage({ jokes }: { jokes: Joke[] }) {
+export default function HomePage({ articles }: { articles: Article[] }) {
   return (
     <div className="flex flex-col">
-      <h1 className="text-3xl font-bold">Jokes</h1>
+      <h1 className="text-3xl font-bold">Articles</h1>
 
       <ul>
-        {jokes.map((joke) => (
-          <li className="ml-5 mt-5" key={joke.id}>
-            <span className="text-xl">{joke.setup}</span>
+        {articles.map((article, index) => (
+          <li className="ml-5 mt-5" key={index}>
+            <a href={article.link} className="text-xl font-bold">{article.title}</a>
             <br />
-            {joke.punchline}
+            <img src={article.img} alt={article.title} className="mt-2" />
           </li>
         ))}
       </ul>
