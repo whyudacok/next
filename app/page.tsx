31124787ba29258b139f498fef9e85b94fb7c9e9
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-07-07 04:05:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import HomePage, { Joke } from './HomePage';
+import HomePage, { Article } from './HomePage';
 
 const title = 'Home | nextjs-starter';
 
@@ -7,13 +7,13 @@ export const metadata: Metadata = {
   title: title,
 };
 
-async function getJokes(): Promise<Joke[]> {
-  const res = await fetch('https://official-joke-api.appspot.com/jokes/ten');
-  const jokes: Joke[] = await res.json();
-  return jokes;
+async function getArticles(): Promise<Article[]> {
+  const res = await fetch('https://cuy-api.vercel.app/v1/home');
+  const data = await res.json();
+  return data.articles;
 }
 
 export default async function Home() {
-  const jokes = await getJokes();
-  return <HomePage jokes={jokes} />;
+  const articles = await getArticles();
+  return <HomePage articles={articles} />;
 }
